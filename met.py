edeebfbd91a9564741506cb9d6a5c46f1a1a2648
joshnsolomon <joshnsolomon@gmail.com>
2024-04-16 19:40:56 -0700

    add icon

--- met.py
+++ met.py
@@ -7,6 +7,8 @@ class Met:
     #PyGame Stuff
     pygame.init()
     screen = pygame.display.set_mode((1280,720))
+    icon = pygame.image.load('./images/icon.png')
+    pygame.display.set_icon(icon)
     clock = pygame.time.Clock()
     small_font = pygame.font.Font('./fonts/Comic Sans MS.ttf', 66)
     medium_font = pygame.font.Font('./fonts/Comic Sans MS.ttf', 100)
